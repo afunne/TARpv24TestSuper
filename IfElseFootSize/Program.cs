c2a8a5add8a7ee71044a196bbc0dc85b65355f44
afunne <tahmazovhussejn@gmail.com>
2025-02-03 16:37:24 +0200

    I AM RWEAKING OUT, THIS IS ########

---
+++ IfElseFootSize/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace IfElseFootSize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor background = Console.BackgroundColor;
+            ConsoleColor foreground = Console.ForegroundColor;
+            int suurus = int.Parse(Console.ReadLine());
+
+            if (suurus >= 30 && suurus <= 33)
+            {
+            Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (suurus >= 34 && suurus <= 38)
+                Console.BackgroundColor = ConsoleColor.White;
+            {
+
+            }
+            if (suurus >= 39 && suurus <= 44)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+            else if (suurus >= 45 && suurus <= 48)
+                Console.Beep();
+            {
+           
+
+
+            }
+            Console.WriteLine("sinu jala suurus on " + suurus);
+        }
+    }
+}
