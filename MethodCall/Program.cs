a8918e05a4d48462aadd9d66f555382446ae635f
afunne <tahmazovhussejn@gmail.com>
2025-02-03 16:44:42 +0200

    ligma kaks

---
+++ MethodCall/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Main Method");
+            //kuidas kutsuda esile Main1 meetod siin:
+            Main1();
+        }
+
+        static void Main1()
+        {
+           Console.WriteLine("Main1 method");
+        }
+    }
+}
