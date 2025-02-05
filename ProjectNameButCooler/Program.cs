b9c118def34721a5b0663132bffbc11438acc2e8
Opilane TTHK <tahmazovhussejn@gmail.com>
2025-02-05 16:22:44 +0200

    for the love of god work pls pls
    GRAAAAAAAAAAAAAAAAAAH I HATE THIS APPLICATION

---
+++ ProjectNameButCooler/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ProjectNameButCooler
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Color c = (Color)(new Random()).Next(0, 5);
+                switch (c)
+                {
+                    case Color.Red:
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Red");
+                        Console.Beep();
+                        break;
+                    case Color.Green:
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Green");
+                        Console.Beep();
+                        break;
+                    case Color.Blue:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Blue");
+                        Console.Beep();
+                        break;
+                    case Color.White:
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("White");
+                        Console.Beep();
+                        break;
+                    case Color.Orange:
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Orange");
+                        Console.Beep();
+                        break;
+                    default:
+                        break;
+                }
+                Console.ResetColor();
+            }
+        }
+        public enum Color
+        {
+            Red, Green, Blue, White, Orange
+        }
+    }
+}
