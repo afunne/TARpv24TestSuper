8b378d529395ea6ba32dbebbf72344aa7a45e46a
afunne <tahmazovhussejn@gmail.com>
2025-02-10 12:33:56 +0200

    YAAAY IT WORKS :333333

---
+++ CalculatorSwitch.cs
@@ -0,0 +1,61 @@
+﻿namespace CalculatorSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Insert first  number and press enter");
+            //string tuleb konvertida int andmetüübiks
+            float firtsNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert calculation type by number");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            float calculatorType = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert second number and press enter");
+            //string tuleb konvertida int andmetüübiks
+            int secondNr = int.Parse(Console.ReadLine());
+
+            switch (calculatorType)
+            {
+                case 1:
+                    float result = firtsNr + secondNr;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine(firtsNr + " + " + secondNr + " = " + result);
+                    break;
+            }
+
+            switch (calculatorType)
+            {
+                case 2:
+                    float result = firtsNr - secondNr;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine(firtsNr + " - " + secondNr + " = " + result);
+                    break;
+            }
+
+            switch (calculatorType)
+            {
+                case 3:
+                    float result = firtsNr * secondNr;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine(firtsNr + " * " + secondNr + " = " + result);
+                    break;
+            }
+
+            switch (calculatorType)
+            {
+                case 4:
+                    float result = firtsNr / secondNr;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine(firtsNr + " / " + secondNr + " = " + result);
+                    break;
+            }
+        }
+}
+}
